6b8f6aec5f8d45747a377cbed9d1ed10720549af
smdpro <samad.rahimi@pinkswan.ch>
2023-03-14 03:21:58 +0330

    adding helper guncs

--- timeslot.go
+++ timeslot.go
@@ -12,7 +12,7 @@ type SlotData struct {
 	End        time.Time
 	Step       int
 	Period     uint
-	DaysInWeek [7]uint
+	DaysInWeek []int
 	Gap        uint
 }
 
@@ -21,10 +21,24 @@ var defaultDate = SlotData{
 	End:        time.Now().UTC().Add(time.Hour),
 	Step:       5,
 	Period:     5,
-	DaysInWeek: [7]uint{1, 2, 3, 4, 5, 6, 7},
+	DaysInWeek: []int{1, 2, 3, 4, 5, 6, 7},
 	Gap:        0,
 }
 
 func SlotGenerator(data SlotData) {
 
+}
+
+
+func index[E comparable](s []E, v E) int {
+	for i, vs := range s {
+		if v == vs {
+			return i
+		}
+	}
+	return -1
+}
+
+func contains[E comparable](s []E, v E) bool {
+	return Index(s, v) >= 0
 }
