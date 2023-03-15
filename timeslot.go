318032fa20f7114d308e2ed036b15cbb7a6cdd74
smdpro <samad.rahimi@pinkswan.ch>
2023-03-16 02:52:41 +0330

    changes

--- timeslot.go
+++ timeslot.go
@@ -1,6 +1,12 @@
 package timeslot
 
-import "time"
+import (
+	"fmt"
+	"time"
+
+	"golang.org/x/exp/slices"
+	"golang.org/x/text/date"
+)
 
 type periods struct {
 	minute string
@@ -11,24 +17,77 @@ type SlotData struct {
 	Start      time.Time
 	End        time.Time
 	Step       int
-	Period     uint
+	Period     int
 	DaysInWeek []int
-	Gap        uint
+	Gap        int
 }
 
 var defaultDate = SlotData{
 	Start:      time.Now().UTC(),
 	End:        time.Now().UTC().Add(time.Hour),
 	Step:       5,
-	Period:     5,
-	DaysInWeek: []int{1, 2, 3, 4, 5, 6, 7},
-	Gap:        0,
+	Period:     1,
+	DaysInWeek: []int{1, 2},
+	Gap:        5,
 }
 
 func SlotGenerator(data SlotData) {
 
-}
+	
+	var start, end time.Time
+	if date.Start == nil {
+		start = defaultDate.Start
+	} else {
+		start = data.Start
+	}
 
+	if date.End == nil {
+		end = defaultDate.End
+	} else {
+		end = data.End
+	}
+	
+
+	step := time.Duration(defaultDate.Step + defaultDate.Gap)
+	current, endTime := start, start
+	pm2am := false
+	for current.Compare(et) < 0 {
+		fmt.Println(int(current.Weekday()))
+
+		current = time.Date(current.Year(), current.Month(), current.Day(), start.Hour(), start.Minute(), 0, 0, time.UTC)
+		endTime = time.Date(current.Year(), current.Month(), current.Day(), end.Hour(), end.Minute(), 0, 0, time.UTC)
+		if start.Hour() > end.Day() {
+			pm2am = true
+			endTime = time.Date(current.Year(), current.Month(), current.Day(), 23, 59, 59, 999, time.UTC)
+		}
+
+		if !contains(defaultDate.DaysInWeek, int(current.Weekday())) {
+			current = current.AddDate(0, 0, 1)
+			continue
+		}
+		for current.Compare(endTime) < 0 {
+
+			fmt.Println(current)
+
+			current = current.Add(time.Minute * step)
+
+		}
+		if pm2am {
+			endTime = time.Date(current.Year(), current.Month(), current.Day(), end.Hour(), end.Minute(), 0, 0, time.UTC)
+			for current.Compare(endTime) < 0 {
+
+				fmt.Println(current)
+
+				current = current.Add(time.Minute * step)
+
+			}
+		}
+		if !pm2am {
+			current = current.AddDate(0, 0, 1)
+		}
+	}
+
+}
 
 func index[E comparable](s []E, v E) int {
 	for i, vs := range s {
